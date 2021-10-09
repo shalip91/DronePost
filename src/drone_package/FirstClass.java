9afb61531639fb10018933e2748ae00a3833b93c
shali persy <shalip91@gmail.com>
2021-10-09 14:19:28 +0300

    second commit

--- src/drone_package/FirstClass.java
+++ src/drone_package/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("hi ludo and oz");
 	}
 
 }
