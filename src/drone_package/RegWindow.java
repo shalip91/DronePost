b53f940a22f81513d7b0a97f0ce5aa763d6e5915
ozpea <ozpea@Oz-Desktop>
2021-10-23 15:33:10 +0300

    Reg window

--- src/drone_package/RegWindow.java
+++ src/drone_package/RegWindow.java
@@ -15,21 +15,24 @@ public class RegWindow extends JFrame implements ActionListener{
 	private JLabel nameLabel = new JLabel("Enter Name: ");
 	private JLabel adressLabel = new JLabel("Enter Adress: ");
 	private JLabel phoneLabel = new JLabel("Enter Phone: ");
+	private JLabel orderQtyLabel = new JLabel("required Shipments: ");
 	private JTextField nameTextField = new JTextField();
 	private JTextField adressTextField = new JTextField();
 	private JTextField phoneTextField = new JTextField();
-	
-	private JButton registerButton = new JButton("Register new user");
+	// List declaration
+	private JList<String> selectionList;  
+
+	private JButton registerButton = new JButton("Register");
 	private int H = 600;
 	private int W = 500;
-	
+
 	public RegWindow() {
 		createMyWindow();
-		setLocation();
 		addComponents();
+		setLocation();
 		actionEventFunc();
 	}
-	
+
 	public void addComponents()
 	{
 		frame.add(titleLabel);
@@ -40,7 +43,16 @@ public class RegWindow extends JFrame implements ActionListener{
 		frame.add(nameTextField);
 		frame.add(adressTextField);
 		frame.add(phoneTextField);
+		frame.add(orderQtyLabel);
+		//add selectionList
+		DefaultListModel<String> l1 = new DefaultListModel<>();  
+		l1.addElement("50(99$)");  
+		l1.addElement("150(179$)");  
+		selectionList = new JList<>(l1);
+		frame.add(selectionList);
+
 	}
+
 	public void createMyWindow()
 	{
 		frame=new JFrame();
@@ -48,44 +60,49 @@ public class RegWindow extends JFrame implements ActionListener{
 		frame.setBounds(0,0,W,H);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(
-				         dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+				dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().setBackground(new Color(240,250,243));
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 	}
-	
-	
-	
+
+
+
 	public void setLocation()
 	{
-		
+
 		titleLabel.setFont(new Font("Serif", Font.BOLD, 40));
 		titleLabel.setBounds(W/12, H/30, (int)(W/1.2), H/10);
 
 		nameLabel.setFont(new Font("Serif", Font.BOLD, 30));
 		nameLabel.setBounds(W/20, (int)(H/4), (int)(W/2), H/6);
-		
+
 		adressLabel.setFont(new Font("Serif", Font.BOLD, 30));
 		adressLabel.setBounds(W/20, (int)(H/4) + (int)(H/10), (int)(W/2), H/6);
-		
+
 		phoneLabel.setFont(new Font("Serif", Font.BOLD, 30));
 		phoneLabel.setBounds(W/20, (int)(H/4) + (int)(H/5), (int)(W/2), H/6);
+
+		orderQtyLabel.setFont(new Font("Serif", Font.BOLD, 30));
+		orderQtyLabel.setBounds(W/20, (int)(H/1.5) , (int)(W/1.1), H/10);
 		
 		registerButton.setFont(new Font("Serif", Font.BOLD, 30));
-		registerButton.setBounds(W/6, (int)(H/1.5), (int)(W/1.5), H/15);
-		
+		registerButton.setBounds(W/6, (int)(H/1.2), (int)(W/1.5), H/15);
+
 		nameTextField.setBounds(W/20 + (int)(W/2.7) , (int)(H/3.5), (int)(W/2), H/10);
 		adressTextField.setBounds(W/20 + (int)(W/2.7), (int)(H/3.5) + (int)(H/9), (int)(W/2), H/10);
 		phoneTextField.setBounds(W/20 + (int)(W/2.7), (int)(H/3.5) + (int)(H/4.5), (int)(W/2), H/10);
-		
+
 
 		nameTextField.setFont(new Font("Serif", Font.BOLD, 30));
 		adressTextField.setFont(new Font("Serif", Font.BOLD, 30));
 		phoneTextField.setFont(new Font("Serif", Font.BOLD, 30));
 
-
+		// selectionList bounds
+		selectionList.setBounds((int)(W/1.7), (int)(H/1.5) , (int)(W/3.5), H/7);
+		selectionList.setFont(new Font("Serif", Font.BOLD, 30));
 	}
 
 	public void actionEventFunc()
@@ -93,18 +110,18 @@ public class RegWindow extends JFrame implements ActionListener{
 		registerButton.addActionListener(this);
 
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		
+
 		if(e.getSource() == registerButton) {
-		frame.dispose();
-		new WelcomeWindow();
-		
-	}
+			frame.dispose();
+			new WelcomeWindow();
+
+		}
 
 	}
-	
-	
+
+
 }
